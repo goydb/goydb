3ed8ca33945f208b6e989f4433dccdbaacc2a982
Vincent Landgraf <vincent@landgrafx.de>
2026-02-15 21:52:39 +0100

    add /_up, /_membership and /_cluster_setup handlers

---
+++ internal/handler/up.go
@@ -0,0 +1,17 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Up struct {
+	Base
+}
+
+func (s *Up) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"status": "ok"}) // nolint: errcheck
+}
