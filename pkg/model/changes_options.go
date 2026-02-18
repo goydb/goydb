429040c823287e4a236dfc028af9377157a05798
Vincent Landgraf <vincent@landgrafx.de>
2026-02-18 21:44:07 +0100

    add FilterServer interface and extend models for filter support

--- pkg/model/changes_options.go
+++ pkg/model/changes_options.go
@@ -3,10 +3,16 @@ package model
 import "time"
 
 type ChangesOptions struct {
-	Since  string
-	Limit  int
-	Timeout time.Duration
-	DocIDs []string
+	Since     string
+	Limit     int
+	Timeout   time.Duration
+	Heartbeat time.Duration
+	DocIDs    []string
+
+	// Filter fields
+	Filter   string                 // Filter type: "_selector", "_view", "_design/ddoc/filtername"
+	Selector map[string]interface{} // For filter=_selector
+	View     string                 // For filter=_view: "_design/ddoc/viewname"
 }
 
 func (o *ChangesOptions) SinceNow() bool {
