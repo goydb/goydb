a651e6321838551ae41a4454213f3f2b36c5f896
Vincent Landgraf <vincent@landgrafx.de>
2026-02-19 20:33:15 +0100

    attachment PUT/DELETE: rev conflict, full id/rev response, Design/Local prefix

--- internal/handler/db_doc_attachment_delete.go
+++ internal/handler/db_doc_attachment_delete.go
@@ -1,13 +1,19 @@
 package handler
 
 import (
+	"encoding/json"
+	"errors"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/goydb/goydb/internal/adapter/storage"
+	"github.com/goydb/goydb/pkg/model"
 )
 
 type DBDocAttachmentDelete struct {
 	Base
+	Design bool
+	Local  bool
 }
 
 func (s *DBDocAttachmentDelete) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -23,14 +29,29 @@ func (s *DBDocAttachmentDelete) ServeHTTP(w http.ResponseWriter, r *http.Request
 	}
 
 	docID := mux.Vars(r)["docid"]
+	if s.Design {
+		docID = string(model.DesignDocPrefix) + docID
+	} else if s.Local {
+		docID = string(model.LocalDocPrefix) + docID
+	}
 	attachment := mux.Vars(r)["attachment"]
 
-	_, err := db.DeleteAttachment(r.Context(), docID, attachment)
-	if err != nil {
+	rev := revFromRequest(r)
+
+	newRev, err := db.DeleteAttachment(r.Context(), docID, attachment, rev)
+	if errors.Is(err, storage.ErrConflict) {
+		WriteError(w, http.StatusConflict, err.Error())
+		return
+	}
+	if errors.Is(err, storage.ErrNotFound) {
 		WriteError(w, http.StatusNotFound, err.Error())
+		return
+	}
+	if err != nil {
+		WriteError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(`{"ok":true}`)) // nolint: errcheck
+	json.NewEncoder(w).Encode(SimpleDocResponse{Ok: true, ID: docID, Rev: newRev}) // nolint: errcheck
 }
